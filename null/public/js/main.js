82cc53d84f629602da2e0737449df61e1edb6319
Dave Voyles <Dvoyles@Microsoft.com>
2017-04-25 18:01:56 -0400

    refactoring

--- null/public/js/main.js
+++ null/public/js/main.js
@@ -1,46 +1,36 @@
 "use strict"; 
 
-var canvas = document.getElementById('demo'),
+var canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d'),
     line = new Line(ctx),
     img = new Image;
 
 ctx.strokeStyle = '#fff';
 
-function Line(ctx) {
-    
-    var me = this;
-    
-    this.x1 = 0;
-    this.x2 = 0;
-    this.y1 = 0;
-    this.y2 = 0;
-    
-    this.draw = function() {
-        ctx.beginPath();
-        ctx.moveTo(me.x1, me.y1);
-        ctx.lineTo(me.x2, me.y2);
-        ctx.stroke();
-    }
-}
 
-function drawRectangle(mouseX,mouseY){
-    var width=mouseX-startX;
-    var height=mouseY-startY;
+
+function drawRectangle(){
+    var startX = 269;
+    var startY = 138;
+    var width  = 40;
+    var height = 40;
+
     ctx.beginPath();
     ctx.rect(startX,startY,width,height);
+    ctx.fillStyle = "green";
     ctx.fill();
     ctx.stroke();
-}
+};
 
 
 img.onload = start;
 img.src    = 'img/hardlight-suit.png';
 
 function start() {
-    ctx.drawImage(img, 0, 0, demo.width, demo.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    drawRectangle();
     canvas.onmousemove = updateLine;
-}
+};
 
 function updateLine(e) {
     var r = canvas.getBoundingClientRect(),
@@ -54,12 +44,11 @@ function updateLine(e) {
     line.x2 = x;
     line.y2 = canvas.height;
     line.draw();
-}
-
-
+};
 
 
 // Debug functions
+/*****************/
 canvas.addEventListener('mousemove', function(evt) {
     var mousePos = getMousePos(canvas, evt);
     var message  = 'X: ' + Number(mousePos.x.toFixed(0)) + ', Y: ' +Number(mousePos.y.toFixed(0));
@@ -67,11 +56,11 @@ canvas.addEventListener('mousemove', function(evt) {
 }, false);
 
 function writeMessage(canvas, message) {
-    var context = canvas.getContext('2d');
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.font = '18pt Calibri';
-    context.fillStyle = 'black';
-    context.fillText(message, 10, 25);
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font      = '18pt Calibri';
+    ctx.fillStyle = 'black';
+    ctx.fillText(message, 10, 25);
 };
 
 function getMousePos(canvas, evt) {
@@ -81,3 +70,18 @@ function getMousePos(canvas, evt) {
         y: evt.clientY - rect.top
     }
 }
+function Line(ctx) {   
+    var me = this;
+    
+    this.x1 = 0;
+    this.x2 = 0;
+    this.y1 = 0;
+    this.y2 = 0;
+    
+    this.draw = function() {
+        ctx.beginPath();
+        ctx.moveTo(me.x1, me.y1);
+        ctx.lineTo(me.x2, me.y2);
+        ctx.stroke();
+    }
+};
