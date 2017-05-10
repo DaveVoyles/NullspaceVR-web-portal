34fa5d502744616f77a4ad7c10c14b1e8eb23478
Dave <startingline13@hotmail.com>
2017-05-10 10:23:21 -0700

    added overlays for all sensors

--- null/public/js/main.js
+++ null/public/js/main.js
@@ -15,29 +15,92 @@ window.onload = function() {
         canvas.onmousemove = updateLine;
     };
 
-   // Starting loc (top-left) when drawing the rectangle
+   /**
+    * Starting loc (top-left) when drawing the rectangle.
+    * Crearte an object to store all of the rectangles here.
+    */
    var rectCoords = [
         this.one = {
-            x: 415,
+            num: "one",
+            x: 400,
             y: 140
         },
         this.two = {
-            x: 269,
+            num: "two",
+            x: 300,
             y: 138 
-        }
+        },
+        this.three = {
+            x: 300,
+            y: 274,
+        },
+        this.four = {
+            x: 400,
+            y: 274,
+        },
+        this.five = {
+            x: 300,
+            y: 338,
+        },
+        this.six = {
+            x: 400,
+            y: 342,
+        },
+        this.seven = {
+            x: 300,
+            y: 418,
+        },
+        this.eight = {
+            x: 400,
+            y: 342,
+        },
+        this.eight = {
+            x: 400,
+            y: 418,
+        },
+        // Shoulders
+        this.nine = {
+            x: 153,
+            y: 30,
+        },
+        this.ten = {
+            x: 400,
+            y: 30,
+        },
+        // Arms
+        this.eleven = {
+            x: 36,
+            y: 179,
+        },
+        this.twelve = {
+            x: 520,
+            y: 179,
+        },
+        this.thirteen = {
+            x: 13,
+            y: 422,
+        },
+        this.thirteen = {
+            x: 560,
+            y: 386,
+        },
     ];
 
 
-    rectCoords.forEach(function(i) {
-        drawRect(ctx, i.x, i.y);
-        console.log("x: "+ i.x);
-        console.log("y: " + i.y);
-    }, this);
+    /** Loop through each set of rectangle coordinates and draw them to the screen.
+     */
+    // rectCoords.forEach(function(i) {
+    //     drawRect(ctx, i.x, i.y);
+    //     console.log('num: ' + i.num);
+    //     console.log("x: " + i.x);
+    //     console.log("y: " + i.y);
+    // }, this);
 
 
-    function drawRect(ctx, startX, startY){
-        var startX = 269;
-        var startY = 138;
+
+        function drawRect(ctx, startXLoc, startYLoc){
+        var startX = startXLoc;
+        var startY = startYLoc;
         var width  = 40;
         var height = 40;
 
@@ -47,6 +110,21 @@ window.onload = function() {
         ctx.fill();
         ctx.stroke();
     };
+
+
+    // OLD FUNCTION - Hardcoded values
+    // function drawRect(ctx, startX, startY){
+    //     var startX = 269;
+    //     var startY = 138;
+    //     var width  = 40;
+    //     var height = 40;
+
+    //     ctx.beginPath();
+    //     ctx.rect(startX, startY, width, height);
+    //     ctx.fillStyle = "green";
+    //     ctx.fill();
+    //     ctx.stroke();
+    // };
 
     function Line(ctx) {   
         var me = this;
@@ -72,9 +150,17 @@ window.onload = function() {
             x = e.clientX - r.left,
             y = e.clientY - r.top;
         
-        // LOOK HERE
+        // LOOK HERE -- seems to be where the drawing gets funky
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        drawRect(ctx);
+        // drawRect(ctx); // OLD
+
+        //TODO: Don't loop through this every frame. Do it only once
+        rectCoords.forEach(function(i) {
+            drawRect(ctx, i.x, i.y);
+            console.log('num: ' + i.num);
+            console.log("x: " + i.x);
+            console.log("y: " + i.y);
+        }, this);
 
         line.x1 = x;
         line.y1 = 0;
